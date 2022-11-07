e40062d8eda2e614ba5a18ac5389aa7315d9bf66
libook <libook7@gmail.com>
2022-11-07 18:56:36 +0800

    feat: add monitor for TranslationSetting; method can be a function

--- translation.js
+++ translation.js
@@ -10,11 +10,19 @@
  */
 
 /**
+ * @typedef {Function} TranslationProcessor
+ * @param {Translation} translation
+ * @param {String} selector
+ * @param {String} description
+ */
+
+/**
  * @typedef {Object} TranslationSetting
  * @property {String} [description] - 描述信息
  * @property {Translation} translation - 翻译数据
  * @property {String} selector - 选择器
- * @property {"static"|"dynamic"} method - 翻译方式
+ * @property {"static"|"dynamic"|TranslationProcessor} method - 翻译方式，如果逻辑复杂可以传入一个TranslationProcessor函数来自己写过程
+ * @property {Array<"attributes"|"childList"|"subtree">} [monitor] - 监听类别
  */
 
 /**
@@ -420,6 +428,282 @@ const translationSettingList = [
             { 'searchWord': 'Table of contents', 'replaceWord': '目录' },
         ],
     },
+    {
+        "description": "左侧菜单栏",
+        "selector": "#launcher-pane",
+        "method": "static",
+        "translation": [
+            { 'searchWord': 'Options', 'replaceWord': '设置选项' },
+            { 'searchWord': 'Open new window', 'replaceWord': '新的窗口' },
+            { 'searchWord': 'Open Dev Tools', 'replaceWord': '开发工具' },
+            { 'searchWord': 'Open SQL Console', 'replaceWord': 'SQL控制台' },
+            { 'searchWord': 'Show backend log', 'replaceWord': '显示后台日志' },
+            { 'searchWord': 'Reload frontend', 'replaceWord': '重新加载页面' },
+            { 'searchWord': 'Toggle fullscreen', 'replaceWord': '切换全屏' },
+            {
+                'searchWord': 'Switch to mobile version',
+                'replaceWord': '切换移动端模式',
+            },
+            {
+                'searchWord': 'Switch to desktop version',
+                'replaceWord': '切换桌面端模式',
+            },
+            { 'searchWord': 'Show Help', 'replaceWord': '显示帮助' },
+            {
+                'searchWord': 'About Trilium Notes',
+                'replaceWord': '关于 Trilium',
+            },
+        ],
+    },
+    {
+        "description": "“关于”对话框",
+        "selector": ".about-dialog",
+        "method": "static",
+        "translation": [
+            {
+                'searchWord': 'About Trilium Notes',
+                'replaceWord': '关于 Trilium 笔记',
+            },
+            { 'searchWord': 'Homepage', 'replaceWord': '项目主页' },
+            { 'searchWord': 'App version', 'replaceWord': '应用版本' },
+            { 'searchWord': 'DB version', 'replaceWord': '数据库版本' },
+            { 'searchWord': 'Sync version', 'replaceWord': '同步版本' },
+            { 'searchWord': 'Build date', 'replaceWord': '编译日期' },
+            { 'searchWord': 'Build revision', 'replaceWord': '编译版本' },
+            { 'searchWord': 'Data directory', 'replaceWord': '数据目录' },
+        ],
+    },
+    {
+        "description": "目录树设置气泡",
+        "selector": ".tree-settings-popup",
+        "method": "static",
+        "translation": [
+            {
+                'searchWord': 'Hide archived notes',
+                'replaceWord': '隐藏已归档笔记',
+            },
+            {
+                'searchWord': 'Hide images included in a note',
+                'replaceWord': '隐藏笔记中的图片',
+            },
+            {
+                'searchWord': 'Automatically collapse notes',
+                'replaceWord': '自动折叠笔记',
+            },
+            {
+                'searchWord': 'Save & apply changes',
+                'replaceWord': '保存并应用修改',
+            },
+        ],
+    },
+    {
+        "description": "文档标签页",
+        "selector": ".split-note-container-widget",
+        "method": (translation, selector, description) => {
+            // 文档标签页
+            /*
+            加载脚本前标签页已经存在；
+            新建标签页切换展示前不加载属性按钮label；
+            新建标签页新增加一套DOM结构无法被事先监视；
+             */
+            const ribbonTabContainerObserverCallback = (mutationList) => {
+                // 对属性栏主按钮的label进行翻译
+                for (const mutation of mutationList) {
+                    translate(
+                        translation,
+                        mutation.target,
+                        description,
+                    );
+                }
+            };
+
+            const splitNoteContainerWidgetObserverCallback = () => {
+                // 找出未加载标签页
+                const newTabList = document.querySelectorAll('.note-split:not(.type-code)');
+                newTabList.forEach(newTab => {
+                    // 对未加载标签页进行翻译
+                    translate(
+                        translation,
+                        newTab.querySelector('.ribbon-container'),
+                        description,
+                    );
+                    // 观察.ribbon-tab-container的childList变化
+                    const ribbonTabContainerObserver = new MutationObserver(ribbonTabContainerObserverCallback);
+                    ribbonTabContainerObserver.observe(newTab.querySelector('.ribbon-tab-container'), {
+                        "attributes": false,
+                        "childList": true,
+                        "subtree": false,
+                    });
+                });
+            };
+
+            // 对已有、已加载的标签页的属性栏进行翻译
+            document.querySelectorAll('.note-split.type-code .ribbon-container').forEach(node => {
+                translate(
+                    translation,
+                    node,
+                    description,
+                );
+            });
+            // 对已有、未加载的标签页的属性栏进行翻译
+            splitNoteContainerWidgetObserverCallback();
+
+            // 观察.split-note-container-widget的childList变化
+            const splitNoteContainerWidgetObserver = new MutationObserver(splitNoteContainerWidgetObserverCallback);
+            splitNoteContainerWidgetObserver.observe(document.querySelector(selector), {
+                "attributes": false,
+                "childList": true,
+                "subtree": false,
+            });
+        },
+        "translation": [
+            { 'searchWord': 'Basic Properties', 'replaceWord': '基本属性' },
+            { 'searchWord': 'Note type', 'replaceWord': '笔记类型' },
+            { 'searchWord': 'Relation Map', 'replaceWord': '关系图' },
+            { 'searchWord': 'Render Note', 'replaceWord': '渲染笔记' },
+            { 'searchWord': 'Mermaid Diagram', 'replaceWord': '美人鱼图' },
+            { 'searchWord': 'Protect the note', 'replaceWord': '保护笔记' },
+            { 'searchWord': 'Editable', 'replaceWord': '允许编辑' },
+            { 'searchWord': 'Bookmark', 'replaceWord': '书签' },
+            { 'searchWord': 'Shared', 'replaceWord': '分享' },
+            {
+                'searchWord': 'Owned attributes',
+                'replaceWord': '拥有的属性',
+            },
+            {
+                'searchWord': 'Inherited attributes',
+                'replaceWord': '继承的属性',
+            },
+            { 'searchWord': 'Note Paths', 'replaceWord': '笔记路径' },
+            {
+                'searchWord': 'This note is placed into the following paths',
+                'replaceWord': '这个笔记已被放到以下路径中',
+            },
+            {
+                'searchWord': 'Clone note to new location...',
+                'replaceWord': '克隆到新路径...',
+            },
+            { 'searchWord': 'Note Map', 'replaceWord': '笔记地图' },
+            { 'searchWord': 'Similar Notes', 'replaceWord': '相似笔记' },
+            { 'searchWord': 'Note Info', 'replaceWord': '笔记信息' },
+
+            {
+                'searchWord': ' Re-render note',
+                'replaceWord': '重新渲染笔记',
+            },
+            {
+                'searchWord': 'Search in note ',
+                'replaceWord': '在笔记中搜索',
+            },
+            { 'searchWord': ' Note source', 'replaceWord': '笔记源代码' },
+            {
+                'searchWord': ' Open note externally',
+                'replaceWord': '用外部程序打开笔记',
+            },
+            { 'searchWord': 'Import files', 'replaceWord': '导入文件' },
+            { 'searchWord': 'Export note', 'replaceWord': '导出笔记' },
+            { 'searchWord': ' Print note', 'replaceWord': '打印笔记' },
+
+
+            {
+                'searchWord': 'Fast search option disables full text search of note contents which might speed up searching in large databases.',
+                'replaceWord': '"快速搜索"选项禁用了笔记内容的全文搜索, 这可能会加快大型数据库中的搜索速度.',
+            },
+
+            {
+                'searchWord': 'Take only first X specified results.',
+                'replaceWord': '只取前N条结果.',
+            },
+            {
+                'searchWord': 'Debug will print extra debugging information into the console to aid in debugging complex queries.',
+                'replaceWord': '调试会将更多调试信息打印到控制台中, 以帮助调试复杂的查询.',
+            },
+            {
+                'searchWord': 'To access the debug information, execute query and click on "Show backend log" in top left corner.',
+                'replaceWord': '.要访问调试信息, 请执行查询, 然后单击左上角的"显示后端日志".',
+            },
+            {
+                'searchWord': 'Search parameters',
+                'replaceWord': '搜索参数',
+            },
+            {
+                'searchWord': 'Add search option:',
+                'replaceWord': '添加搜索选项：',
+            },
+            {
+                'searchWord': 'Search string:',
+                'replaceWord': '搜索关键词：',
+            },
+            { 'searchWord': 'Search script:', 'replaceWord': '搜索脚本：' },
+
+            { 'searchWord': 'Ancestor:', 'replaceWord': '起始层级：' },
+            { 'searchWord': 'depth:', 'replaceWord': '深度：' },
+            { 'searchWord': "doesn\\'t mattter", 'replaceWord': '任意' },
+            { 'searchWord': 'is exactly ', 'replaceWord': '等于 ' },
+            {
+                'searchWord': '\\(direct children\\)',
+                'replaceWord': '(直接子类) ',
+            },
+            { 'searchWord': 'is greater than ', 'replaceWord': '大于 ' },
+            { 'searchWord': 'is less than ', 'replaceWord': '小于' },
+            { 'searchWord': 'Fast search', 'replaceWord': '快速搜索' },
+            {
+                'searchWord': 'Include archived notes',
+                'replaceWord': '包含已归档笔记',
+            },
+            { 'searchWord': 'Order by', 'replaceWord': '排序' },
+            { 'searchWord': 'Limit', 'replaceWord': '取前N个' },
+            { 'searchWord': 'Debug', 'replaceWord': '调试' },
+
+            {
+                'searchWord': 'Relevancy \\(default\\)',
+                'replaceWord': '相关性 (默认)',
+            },
+            { 'searchWord': 'Title', 'replaceWord': '标题' },
+            { 'searchWord': 'Date created', 'replaceWord': '创建日期' },
+            {
+                'searchWord': 'Date of last modification',
+                'replaceWord': '最后修改日期',
+            },
+            {
+                'searchWord': 'Note content size including revisions',
+                'replaceWord': '笔记内容大小(包括历史)',
+            },
+            {
+                'searchWord': 'Note content size',
+                'replaceWord': '笔记内容大小',
+            },
+            {
+                'searchWord': 'Number of revisions',
+                'replaceWord': '历史数量',
+            },
+            {
+                'searchWord': 'Number of children notes',
+                'replaceWord': '子笔记数',
+            },
+            { 'searchWord': 'Number of clones', 'replaceWord': '克隆数' },
+            { 'searchWord': 'Number of labels', 'replaceWord': '标签数量' },
+            {
+                'searchWord': 'Number of relations targeting the note',
+                'replaceWord': '指向笔记的关系数',
+            },
+            {
+                'searchWord': 'Number of relations',
+                'replaceWord': '关系数',
+            },
+            { 'searchWord': 'Random order', 'replaceWord': '随机顺序' },
+            {
+                'searchWord': 'Ascending \\(default\\)',
+                'replaceWord': '升序（默认）',
+            },
+            { 'searchWord': 'Descending', 'replaceWord': '降序' },
+
+            {
+                'searchWord': "Open a note by typing the note's title into the input below or choose a note in the tree.",
+                'replaceWord': '在下方输入笔记名称或者点树形图来打开笔记.',
+            },
+        ],
+    },
 ];
 
 // 将长的文本排在前面，避免长文本包含短文本导致匹配失败的问题。
@@ -439,14 +723,8 @@ const tagNameListExcluded = [
 ];
 
 /**
- * 观察器的配置，仅观察每个Tab页元素的子节点列表是否有变化。
- * 当Options对话框显示的时候会启动生成每个标签页的内容，此时触发观察器。
- * @type {{subtree: boolean, attributes: boolean, childList: boolean}}
- */
-const observerConfig = { attributes: false, childList: true, subtree: false };
-
-/**
- * 递归扫描节点树
+ * 递归扫描节点树替换文本
+ * 不要直接调用这个函数，请使用translate和observeAndTranslate
  * @param {Translation} translation
  * @param {Element|Node} node
  */
@@ -459,43 +737,109 @@ const recurveSearchNodeAndReplaceText = (translation, node) => {
         if (!tagNameListExcluded.includes(node.tagName)) {
             for (const { searchWord, replaceWord } of translation) {
                 const reg = new RegExp(searchWord, 'g');
-                node.textContent = node.textContent.replace(reg, replaceWord);
+                if (node.textContent.trim() !== '' && reg.test(node.textContent))
+                    node.textContent = node.textContent.replace(reg, replaceWord);
             }
         }
     }
 };
 
-const watchAndTranslate = (translation, node) => {
+/**
+ * 立即翻译
+ * @param translation
+ * @param node
+ * @param description
+ */
+const translate = (translation, node, description) => {
+    console.log('执行静态翻译：', description, node);
+    recurveSearchNodeAndReplaceText(translation, node);
+};
+
+/**
+ * 观察，对发生变化的部分进行翻译
+ * @param translation
+ * @param node
+ * @param description
+ * @param monitor
+ */
+const observeAndTranslate = (translation, node, description, monitor = ["childList"]) => {
     /**
      * @param {Array<MutationRecord>} mutationList
      */
     const mutationCallback = (mutationList) => {
-        console.log(mutationList);
-
         mutationList.forEach((mutation) => {
-            if (mutation.type === 'childList') {
-                console.log(`Options ${mutation.target.id} Tab的翻译执行`);
-                recurveSearchNodeAndReplaceText(translation, mutation.target);
-            }
+            console.log('执行动态翻译：', description, mutation.target);
+            recurveSearchNodeAndReplaceText(translation, mutation.target);
         });
     };
 
-// 创建一个观察器实例并传入回调函数
+    // 创建一个观察器实例并传入回调函数
     const observer = new MutationObserver(mutationCallback);
+
+    /**
+     * 观察器的配置，仅观察每个Tab页元素的子节点列表是否有变化。
+     * 当Options对话框显示的时候会启动生成每个标签页的内容，此时触发观察器。
+     * @type {{subtree: boolean, attributes: boolean, childList: boolean}}
+     */
+    const observerConfig = {
+        "attributes": monitor.includes('attributes'),
+        "childList": monitor.includes('childList'),
+        "subtree": monitor.includes('subtree'),
+    };
+
+    {
+        const { attributes, childList, subtree } = observerConfig;
+        if (!(attributes || childList || subtree)) {
+            debugger
+        }
+    }
 
     observer.observe(node, observerConfig);
 };
 
-for (const { method, selector, translation } of translationSettingList) {
+for (const {
+    method,
+    monitor,
+    selector,
+    translation,
+    description,
+} of translationSettingList) {
     const nodeList = document.querySelectorAll(selector);
     switch (method) {
         case "static": {
-            nodeList.forEach(node => recurveSearchNodeAndReplaceText(translation, node));
+            nodeList.forEach(node => translate(translation, node, description));
             break;
         }
         case "dynamic": {
-            nodeList.forEach(node => watchAndTranslate(translation, node));
+            nodeList.forEach(node => observeAndTranslate(translation, node, description, monitor));
             break;
         }
+        default: {
+            if (typeof method === 'function') {
+                method(translation, selector, description);
+            }
+        }
     }
+}
+
+{
+    //在关于里面添加翻译者的github链接
+    //这可以让您更方便地找到我，并获取最新翻译的更新
+    const tempContainer = document.createElement('tbody');
+    tempContainer.innerHTML = `
+        <tr>
+            <th>翻译者:</th>
+            <td><a href="https://github.com/lioooooh/trilium-translation">柒元... github</a></td>
+        </tr>
+        <tr>
+            <th>参考翻译:</th>
+            <td><a href="https://github.com/Nriver/trilium-translation">https://github.com/Nriver/trilium-translation</a></td>
+        </tr>
+        <tr>
+            <th>重要感谢:</th>
+            <td>感谢Nriver的翻译让我有机会认识和理解这个笔记，感谢您对trilium的贡献</td>
+        </tr>
+    `;
+    const aboutElement = document.querySelector('.about-dialog tbody');
+    Array.from(tempContainer.children).forEach(node => aboutElement.append(node));
 }
